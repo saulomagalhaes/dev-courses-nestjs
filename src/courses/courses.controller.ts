a41d872777116585850a81250eee8d6406412d60
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2022-12-13 12:16:09 -0300

    feat: :tada: inicia projeto em nestjs

---
+++ src/courses/courses.controller.ts
@@ -0,0 +1,44 @@
+import { UpdateCourseDto } from './dto/update-course.dto';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  HttpCode,
+  HttpStatus,
+  Delete,
+  Put,
+} from '@nestjs/common';
+import { CoursesService } from './courses.service';
+import { CreateCourseDto } from './dto/create-course.dto';
+@Controller('courses')
+export class CoursesController {
+  constructor(private readonly coursesService: CoursesService) {}
+
+  @Get()
+  findAll() {
+    return this.coursesService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.coursesService.findOne(id);
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.NO_CONTENT)
+  create(@Body() createCourseDto: CreateCourseDto) {
+    return this.coursesService.create(createCourseDto);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateCourseDto: UpdateCourseDto) {
+    return this.coursesService.update(id, updateCourseDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.coursesService.remove(id);
+  }
+}

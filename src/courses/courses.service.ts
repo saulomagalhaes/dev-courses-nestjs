a8224b5f2d92be32944dcb6ece7e175665e4904d
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2022-12-14 12:18:08 -0300

    refactor: :recycle: refatora o courses.services para que os métodos tenham um retorno adqueado

--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -4,20 +4,28 @@ import { Repository } from 'typeorm';
 import { CreateCourseDto } from './dto/create-course.dto';
 import { UpdateCourseDto } from './dto/update-course.dto';
 import { Course } from './entities/course.entity';
+import { Tag } from './entities/tag.entity';
 
 @Injectable()
 export class CoursesService {
   constructor(
     @InjectRepository(Course)
     private readonly courseRepository: Repository<Course>,
+
+    @InjectRepository(Tag)
+    private readonly tagRepository: Repository<Tag>,
   ) {}
 
   findAll() {
-    return this.courseRepository.find();
+    return this.courseRepository.find({
+      relations: ['tags'],
+    });
   }
 
   findOne(id: string) {
-    const course = this.courseRepository.findOne(id);
+    const course = this.courseRepository.findOne(id, {
+      relations: ['tags'],
+    });
 
     if (!course) {
       throw new NotFoundException(`Course ID ${id} not found`);
@@ -25,14 +33,28 @@ export class CoursesService {
     return course;
   }
 
-  create(createCourseDto: CreateCourseDto) {
-    const course = this.courseRepository.create(createCourseDto);
+  async create(createCourseDto: CreateCourseDto) {
+    const tags = await Promise.all(
+      createCourseDto.tags.map((name) => this.preloadTagByName(name)),
+    );
+
+    const course = this.courseRepository.create({
+      ...createCourseDto,
+      tags,
+    });
     return this.courseRepository.save(course);
   }
   async update(id: string, updateCourseDto: UpdateCourseDto) {
+    const tags =
+      updateCourseDto.tags &&
+      (await Promise.all(
+        updateCourseDto.tags.map((name) => this.preloadTagByName(name)),
+      ));
+
     const course = await this.courseRepository.preload({
       id: +id,
       ...updateCourseDto,
+      tags,
     });
     if (!course) {
       throw new NotFoundException(`Course ID ${id} not found`);
@@ -45,5 +67,13 @@ export class CoursesService {
       throw new NotFoundException(`Course ID ${id} not found`);
     }
     return this.courseRepository.remove(course);
+  }
+
+  private async preloadTagByName(name: string): Promise<Tag> {
+    const tag = await this.tagRepository.findOne({ name });
+    if (tag) {
+      return tag;
+    }
+    return this.tagRepository.create({ name });
   }
 }

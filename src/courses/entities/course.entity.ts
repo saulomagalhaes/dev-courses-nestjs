5edba8d395a6fe1c882936102c7d3a9b315a2651
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2022-12-14 12:04:49 -0300

    feat: :sparkles: altera a course.entity para relacionamento many to many

--- src/courses/entities/course.entity.ts
+++ src/courses/entities/course.entity.ts
@@ -1,13 +1,24 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Tag } from './tag.entity';
 
 @Entity('courses')
 export class Course {
   @PrimaryGeneratedColumn()
   id: number;
+
   @Column()
   name: string;
+
   @Column()
   description: string;
-  @Column('json', { nullable: true })
-  tags: string[];
+
+  @JoinTable()
+  @ManyToMany(() => Tag, (tag) => tag.courses, { cascade: true })
+  tags: Tag[];
 }
